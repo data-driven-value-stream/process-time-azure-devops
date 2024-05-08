2cdbfe4755984819c82d9fc2f38d9f083b61ab36
Andrey Kniazev <worldpwn@hotmail.com>
2024-05-08 17:04:50 +0200

    Add more test cases

--- tests/test_get_last_attempt_to_deliver.py
+++ tests/test_get_last_attempt_to_deliver.py
@@ -8,9 +8,33 @@ def test_only_current_run_exist_should_return_current_run():
     result = get_last_attempt_to_deliver(pipelines)
     assert result == current_run
 
-def test_if_previous_run_was_successful_return_current_run():
+def test_previous_run_was_successful_return_current_run():
     current_run = generate_test_run(3, 'succeeded')
     previous_run = generate_test_run(2, 'succeeded')
     pipelines = [current_run, previous_run]
     result = get_last_attempt_to_deliver(pipelines)
     assert result == current_run
+
+def test_previous_run_was_failed_but_no_more_runs_return_current_run():
+    current_run = generate_test_run(3, 'succeeded')
+    previous_run = generate_test_run(2, 'failed')
+    pipelines = [current_run, previous_run]
+    result = get_last_attempt_to_deliver(pipelines)
+    assert result == current_run
+
+def test_previous_run_was_failed_return_last_successful():
+    current_run = generate_test_run(3, 'succeeded')
+    previous_run = generate_test_run(2, 'failed')
+    previous_run2 = generate_test_run(1, 'succeeded')
+    pipelines = [current_run, previous_run, previous_run2]
+    result = get_last_attempt_to_deliver(pipelines)
+    assert result == previous_run2
+
+def test_two_previous_runs_both_failed_return_last_successful():
+    current_run = generate_test_run(3, 'succeeded')
+    previous_run = generate_test_run(2, 'failed')
+    previous_run2 = generate_test_run(1, 'failed')
+    previous_run3 = generate_test_run(0, 'succeeded')
+    pipelines = [current_run, previous_run, previous_run2, previous_run3]
+    result = get_last_attempt_to_deliver(pipelines)
+    assert result == previous_run3
