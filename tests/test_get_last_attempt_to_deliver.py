1ff9423cce96a7e7376964016a11c60b4266c5a0
Andrey Kniazev <worldpwn@hotmail.com>
2024-05-08 16:59:56 +0200

    create helper function

--- tests/test_get_last_attempt_to_deliver.py
+++ tests/test_get_last_attempt_to_deliver.py
@@ -1,16 +1,16 @@
 from azure.devops.v7_1.pipelines.models import Run
 from src.parsers.get_last_attempt_to_deliver import get_last_attempt_to_deliver
-
+from tests.generate_test_run import generate_test_run
 
 def test_only_current_run_exist_should_return_current_run():
-    current_run = Run(3, 'delivery pipeline', None, None, 'cd.yml', None, None, None, "succeeded", None, None, None, None )
+    current_run = generate_test_run(3, 'succeeded')
     pipelines = [current_run]
     result = get_last_attempt_to_deliver(pipelines)
     assert result == current_run
 
 def test_if_previous_run_was_successful_return_current_run():
-    current_run = Run(3, 'delivery pipeline', None, None, 'cd.yml', None, None, None, "succeeded", None, None, None, None )
-    previous_run = Run(2, 'delivery pipeline', None, None, 'cd.yml', None, None, None, "succeeded", None, None, None, None )
+    current_run = generate_test_run(3, 'succeeded')
+    previous_run = generate_test_run(2, 'succeeded')
     pipelines = [current_run, previous_run]
     result = get_last_attempt_to_deliver(pipelines)
     assert result == current_run
