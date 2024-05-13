dc8c4499ad796ab777ee2f44f4c124d766c51225
Andrey Kniazev <worldpwn@hotmail.com>
2024-05-13 10:08:21 +0200

    remove custom api

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,9 @@
-from api.AzureDevOpsApi import AzureDevOpsApi
 
-api = AzureDevOpsApi("test")
-
-pipelineRuns = api.getPipelineRuns("test", "test", "test")
 
 
 
 print('hello world 2')
 
 
+if __name__ = "__main__":
+    print('hello world 3')
