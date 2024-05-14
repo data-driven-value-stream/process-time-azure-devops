39f81820a2944a0b89769c38ffa003ec0238dbdd
Andrey Kniazev <worldpwn@hotmail.com>
2024-05-14 11:00:56 +0200

    Add logic to include run id

--- src/process_time_azure_devops/parsers/get_last_attempt_to_deliver.py
+++ src/process_time_azure_devops/parsers/get_last_attempt_to_deliver.py
@@ -3,18 +3,19 @@ import json
 from azure.devops.v7_1.pipelines.models import Run
 
 
-def get_last_attempt_to_deliver(runs: [Run]) -> Run:
+def get_last_attempt_to_deliver(current_run_id: int, runs: [Run]) -> Run:
     """Get the last attempt to deliver from the list of pipelines."""
-    print(json.dumps([run.as_dict() for run in runs], sort_keys=True, indent=4))
-    if len(runs) == 1:
-        return runs[0]
-    failed_runs = amount_of_failed_previous_runs(runs)
+
+    sliced_runs = slice_until_current_run_id(current_run_id, runs)
+    if len(sliced_runs) == 1:
+        return sliced_runs[0]
+    failed_runs = amount_of_failed_previous_runs(sliced_runs)
     if failed_runs == 0:
-        return runs[0]
+        return sliced_runs[0]
     else:
-        if len(runs) == failed_runs:
-            return runs[0]
-        return runs[failed_runs]
+        if len(sliced_runs) == failed_runs:
+            return sliced_runs[0]
+        return sliced_runs[failed_runs]
 
 
 def amount_of_failed_previous_runs(runs: [Run]) -> int:
@@ -26,3 +27,13 @@ def amount_of_failed_previous_runs(runs: [Run]) -> int:
         else:
             count += 1
     return count
+
+
+def slice_until_current_run_id(current_run_id: id, runs: [Run]):
+    try:
+        if runs[0].id == current_run_id:
+            return runs
+        index = next(i for i, run in enumerate(runs) if run.id == current_run_id)
+    except StopIteration:
+        return []
+    return runs[index:]
