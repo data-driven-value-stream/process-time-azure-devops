3601f6db0807af1380d87fa4523eecb328f48e00
Andrey Kniazev <worldpwn@hotmail.com>
2024-06-21 12:53:41 +0200

    Use new way of getting process time

--- src/process_time_azure_devops/__main__.py
+++ src/process_time_azure_devops/__main__.py
@@ -2,7 +2,6 @@ from azure.devops.v7_1.pipelines.pipelines_client import PipelinesClient
 from azure.devops.v7_1.build.build_client import BuildClient
 from azure.devops.v7_1.git.git_client import GitClient
 from azure.devops.v7_1.git.models import GitPullRequestQuery, GitPullRequestQueryInput
-from azure.devops.v7_1.build.models import Build
 from process_time_azure_devops.parsers.get_last_attempt_to_deliver import get_last_attempt_to_deliver
 from process_time_azure_devops.models.ArgumentParseResult import ArgumentParseResult
 from process_time_azure_devops.models.JsonResult import JsonResult
@@ -13,7 +12,6 @@ from msrest.authentication import BasicAuthentication
 import getopt
 import sys
 import json
-import datetime
 
 
 def display_help():
@@ -51,20 +49,6 @@ def parse_arguments(argv) -> ArgumentParseResult:
     print(f'Current Run ID: {current_run_id}')
     print('================================')
     return ArgumentParseResult(azure_devops_organization, personal_access_token, project, pipeline_id, current_run_id)
-
-
-def get_first_commit_date(args: ArgumentParseResult, query_result, git_client: GitClient, commit: str, build: Build) -> datetime.datetime:
-    pr = find_pr(project=args.project, query_result=query_result, git_client=git_client, commit=commit, build=build)
-    # If pr_start_time is None it means that run is caused by a commit not in a pull request
-    if pr is None:
-        print('No pull request found for the commit')
-        commit_info = git_client.get_commit(commit, build.repository.id, args.project)
-        print('Commit info:')
-        print(json.dumps(commit_info.as_dict(), sort_keys=True, indent=4))
-        first_commit_time = commit_info.author.date
-        print(f'First commit time: {first_commit_time}')
-        return commit_info.author.date
-    return get_first_commit_date_from_pr(pr)
 
 
 def calculate_process_time(args: ArgumentParseResult) -> JsonResult:
@@ -106,7 +90,18 @@ def calculate_process_time(args: ArgumentParseResult) -> JsonResult:
     print(json.dumps(query_result.as_dict(), sort_keys=True, indent=4))
 
     # If query result is empty it means that run is caused by a commit not in a pull request
-    first_commit_date = get_first_commit_date(args, query_result, git_client, commit, build)
+    # find first pr
+    pr = find_pr(project=args.project, query_result=query_result, git_client=git_client, commit=commit, build=build)
+    if pr is None:
+        print('No pull request found for the commit')
+        commit_info = git_client.get_commit(commit, build.repository.id, args.project)
+        print('Commit info:')
+        print(json.dumps(commit_info.as_dict(), sort_keys=True, indent=4))
+        first_commit_time = commit_info.author.date
+        print(f'First commit time: {first_commit_time}')
+        first_commit_date = commit_info.author.date
+    else:
+        first_commit_date = get_first_commit_date_from_pr(pr)
 
     # Get time difference between first commit and deployment
     current_run = build_client.get_build(args.project, args.current_run_id)
@@ -123,7 +118,7 @@ def calculate_process_time(args: ArgumentParseResult) -> JsonResult:
         process_time_in_minutes=process_time.total_seconds() / 60,
         production_build_id=build.id,
         production_build_url="",
-        first_change_pull_request_id=query_result.results[0].,
+        first_change_pull_request_id=query_result.results[0],
         first_change_pull_request_url="",
     )
 
@@ -137,4 +132,3 @@ if __name__ == "__main__":
     print('========== Result: ==========')
     print(process_time_result.to_json())
     print('=============================')
-
