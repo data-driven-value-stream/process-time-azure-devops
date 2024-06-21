96284b97716ebf5290568c06b7b394ec8fde3a1f
Andrey Kniazev <worldpwn@hotmail.com>
2024-06-21 16:02:43 +0200

    Add  new param to determine trunk based

--- src/process_time_azure_devops/models/ArgumentParseResult.py
+++ src/process_time_azure_devops/models/ArgumentParseResult.py
@@ -1,5 +1,14 @@
 class ArgumentParseResult:
-    def __init__(self, azure_devops_organization: str, personal_access_token: str, project: str, pipeline_id: int, current_run_id: int):
+    def __init__(self,
+                 azure_devops_organization: str,
+                 personal_access_token: str,
+                 project: str,
+                 pipeline_id: int,
+                 current_run_id: int,
+                 production_branch_name: str | None):
+        """
+         :param str | None production_branch_name: Is used to determine if it is a GitFlow, if it is a Trunk Based should be None
+        """
         self.azure_devops_organization = azure_devops_organization
         self.personal_access_token = personal_access_token
         self.project = project
