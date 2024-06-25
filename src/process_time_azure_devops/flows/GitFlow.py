185b42c342a37191b78e8dd2b852a2efb5bd3f88
Andrey Kniazev <worldpwn@hotmail.com>
2024-06-25 13:20:59 +0200

    check if can find current buikd

--- src/process_time_azure_devops/flows/GitFlow.py
+++ src/process_time_azure_devops/flows/GitFlow.py
@@ -34,6 +34,29 @@ class GitFlow(Flow):
         print('Current Build info:')
         print(json.dumps(build.as_dict(), sort_keys=True, indent=4))
 
+
+        # IDEA:
+        # Get current BUILD from production branch
+        # Get previous BUILD from production branch
+        # Look for the RUNS from development branch between them by id
+        # If none find look for previous previous build from production branch
+        # Repeat until find the build from development branch.
+
+
+        # Doing IDEA:
+        prod_branches_builds = (build_client.get_builds(self.args.project,
+                                          definitions=[self.args.pipeline_id],
+                                          branch_name=f"refs/heads/{self.args.production_branch_name}"))
+        # Find the previous build from production branch
+        # Get index of the current build
+        current_build = next((build for build in prod_branches_builds if build.id == self.args.current_run_id),
+                                   None)
+        if current_build is None:
+            raise ValueError(f'Current build not found in production branch {self.args.current_run_id}')
+
+        print(json.dumps(current_build.as_dict(), sort_keys=True, indent=4))
+
+
         # Get pipeline runs
         # pipelines_client = PipelinesClient(url, credentials)
         # runs = pipelines_client.list_runs(self.args.project, self.args.pipeline_id)
@@ -41,10 +64,10 @@ class GitFlow(Flow):
         #     print(json.dumps(run.as_dict(), sort_keys=True, indent=4))
 
         # Get builds that where source_branch == development_branch_name
-        builds = (build_client.get_builds(self.args.project,
-                                         definitions=[self.args.pipeline_id],
-                                         branch_name=f"refs/heads/{self.args.development_branch_name}"))
-        print(f'Builds info (source_branch {self.args.development_branch_name})')
-        for b in builds:
-            print(json.dumps(b.as_dict(), sort_keys=True, indent=4))
-        return 1
+        # builds = (build_client.get_builds(self.args.project,
+        #                                  definitions=[self.args.pipeline_id],
+        #                                  branch_name=f"refs/heads/{self.args.development_branch_name}"))
+        # print(f'Builds info (source_branch {self.args.development_branch_name})')
+        # for b in builds:
+        #     print(json.dumps(b.as_dict(), sort_keys=True, indent=4))
+        # return 1
